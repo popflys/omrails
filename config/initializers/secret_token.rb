b61905def5d3ae14d7c8009002620ce2dc30f830
Andrew Rutledge <drew@popflys.com>
2013-03-19 19:54:21 +0100

    Initial Commit DAWG

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Omrails::Application.config.secret_token = '32c5cf8d8523d76d8c25777b56ed08404a325570963350e3208dfbd838a6ee7ea3a54a0f35b7e140636c08d4724553c6b869632ed33b1d432a771390f6d3da3f'
