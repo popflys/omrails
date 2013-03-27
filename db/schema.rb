b1d4e1c469f93d4d86da10ccd6f6c55f91655946
Andrew Rutledge <drew@popflys.com>
2013-03-27 13:44:12 +0100

    added basic pin structure

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130325214911) do
+ActiveRecord::Schema.define(:version => 20130327122339) do
+
+  create_table "pins", :force => true do |t|
+    t.string   "description"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -27,6 +33,7 @@ ActiveRecord::Schema.define(:version => 20130325214911) do
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
     t.string   "name"
+    t.string   "zip"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
